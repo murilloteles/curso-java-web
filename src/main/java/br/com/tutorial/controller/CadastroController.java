fee6e640b3f9581aa52ef59c9baf13dd15cb842e
Murillo Teles <murilloroquete@gmail.com>
2015-11-13 23:25:22 -0200

    Atualizando controller e cadastro

--- src/main/java/br/com/tutorial/controller/CadastroController.java
+++ src/main/java/br/com/tutorial/controller/CadastroController.java
@@ -7,20 +7,44 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
 
 import br.com.tutorial.model.CadastroDTO;
-import br.com.tutorial.model.dao.CadastroDAO;
 import br.com.tutorial.model.entity.Categoria;
 import br.com.tutorial.model.entity.UF;
+import br.com.tutorial.model.service.CadastroAnuncioService;
 
 
 @ManagedBean(name="cadastroController")
 @RequestScoped
 public class CadastroController implements Serializable {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private CadastroDTO dto = new CadastroDTO();
+
+	private CadastroAnuncioService service = new CadastroAnuncioService();
+
+	public List<UF> getUfs(){
+		return service.getUfs();
+	}
+
+	public List<Categoria> getCategorias(){
+		return service.getCategoria();
+	}
+
+	public String salvar(){
+		service.inserirAnuncio(dto);
+		return "index";
+	}
+
+	public CadastroAnuncioService getService() {
+		return service;
+	}
+
+
+	public void setService(CadastroAnuncioService service) {
+		this.service = service;
+	}
 
 
 	public CadastroDTO getDto() {
@@ -32,20 +56,4 @@ public class CadastroController implements Serializable {
 		this.dto = dto;
 	}
 
-	public List<UF> getUfs(){
-		CadastroDAO dao = new CadastroDAO();
-		return dao.getUfs();
-	}
-	
-	public List<Categoria> getCategorias(){
-		CadastroDAO dao = new CadastroDAO();
-		return dao.getCategorias();
-	}
-	
-	public String salvar(){
-		CadastroDAO dao = new CadastroDAO();
-		dao.salvarAnuncio(dto.getAnuncio());
-		return "index";
-	}
-	
 }
